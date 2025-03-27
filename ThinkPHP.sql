6294c6f4e5774f6a6b395edb2e087107404dc923
KLenesley <lenesley.kylian@ik.me>
2025-03-27 11:08:07 +0100

    changement form

--- ThinkPHP.sql
+++ ThinkPHP.sql
@@ -17,7 +17,7 @@
 
 -- Listage de la structure de la base pour thinkphp
 DROP DATABASE IF EXISTS `thinkphp`;
-CREATE DATABASE IF NOT EXISTS `thinkphp` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE IF NOT EXISTS `thinkphp`;
 USE `thinkphp`;
 
 -- Listage de la structure de la table thinkphp. quiz_answers
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS `quiz_answers` (
   `id` int NOT NULL AUTO_INCREMENT,
   `answer` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
 -- Listage des données de la table thinkphp.quiz_answers : 2 rows
 DELETE FROM `quiz_answers`;
--- main.php
+++ main.php
@@ -13,13 +13,18 @@ $dotenv->load(__DIR__.'/.env');
 
 log_action('Debut du programme');
 
-
 $loader = new \Twig\Loader\FilesystemLoader('templates/');
 $twig = new \Twig\Environment($loader);
 
-echo $twig->render('quiz_form.html.twig', [
-    'Q1' => 'Miss Fortune',
-    'Q2' => 'Miss Fortune',
+$A1 = ['Paris', 'Lyon', 'Marseille', 'Toulouse'];
+$A2 = ['Berlin', 'Munich', 'Hambourg', 'Francfort'];
+
+
+echo $twig->render('quiz_form.html', [
+    'Q1' => 'Quelle est la capitale de la France ?',
+    'Q2' => 'Quelle est la capitale de l\'Allemagne ?',
+    'A1' => $A1,
+    'A2' => $A2,
 ]);
 
 /* Utilisation de twig
